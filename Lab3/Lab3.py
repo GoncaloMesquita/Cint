9283338f9da5862e26189586eaa89bd964ea9ff4
GoncaloMesquita <goncalos.mesq543@gmail.com>
2022-09-27 11:55:34 +0100

    ABC

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -1,34 +1,62 @@
-from re import A
-import numpy as np
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 from statistics import stdev
+
 def identify_outliers(data):
 
-    print(data.Low.std())
-    print(data.Low.mean())
-    print(data.Low.describe())
+    #print(data.Low.describe())
     upper_limit = data.Low.mean() + 3*data['Low'].std()
     lower_limit = data.Low.mean() - 3*data['Low'].std()
 
-    data_out = data[(data.Low > upper_limit) | (data.Low < lower_limit)]
-    
+    out = data[(data.Low > upper_limit) | (data.Low < lower_limit)] 
+    return out
 
+def taking_outliers(data, outlier):
 
-#def remove_outliers():
+    data_clean = data['Low'].drop(outlier.index[0], inplace = False)
+    data_clean.plot()
+    plt.show()
+    return
 
+def define_outlier_previous(data, outlier):
+
+    data['Low'] = data['Low'].replace(outlier.index[0], data['Low'][outlier.index[0]-1])
+    print(data['Low'][100:110])
+#EXERCISE 1
 
 df = pd.read_csv('EURUSD_Daily_Ask_2018.12.31_2019.10.05v2.csv', sep = ';', decimal=",")
 
-df['Low'].plot()
+df.plot()
 plt.show()
 
 Time = pd.to_datetime(df['Time (UTC)'])
 startTime = datetime.datetime(2022, 9, 23, 0, 0)
-identify_outliers(df)
+outliers = identify_outliers(df)
+taking_outliers(df, outliers)
+define_outlier_previous(df, outliers)
 
+#EXERCISE 2
 
-    
+df1 = pd.read_csv('DCOILBRENTEUv2.csv')
+df1.DATE = pd.to_datetime(df1.DATE)
 
-# %%
+df1.plot(x = 'DATE')
+plt.show()
+
+df1['DCOILBRENTEU'].diff().hist(bins=35)
+plt.show()
+
+df1['DCOILBRENTEU'].groupby(df1['DATE'].dt.year).count().plot(kind="bar")
+plt.show()
+
+#EXERCICE 3 
+
+df2 = pd.read_csv('DCOILWTICOv2.csv')
+print(df2)
+
+ax = df1['DCOILBRENTEU'].plot()
+
+df2['DCOILWTICO'].plot(ax=ax)
+plt.show()

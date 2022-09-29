6198bab2e2230ff0ee3cc111d7d843a5db442b48
Duarte <duarte3391@gmail.com>
2022-09-29 12:13:34 +0100

    'duarte'

--- Lab4/Lab4_duarte.py
+++ Lab4/Lab4_duarte.py
@@ -11,9 +11,13 @@ hab_dat.columns = ['Age', 'Year of operation', 'Nodes detected', 'Survival statu
 
 # Iris
 iris_dat = pd.read_table('iris.data',sep=',',header=None)
-iris_dat.columns = ['sepal length','sepal width','petal length','petal length','class']
+iris_dat.columns = ['sepal length','sepal width','petal length','petal width','class']
+
+
 
 
 sns.set_theme(style="darkgrid")
-sns.relplot(iris_dat,x='sepal length',y='sepal width',hue='class',style='class')
-# plt.show()
+sns.relplot(iris_dat,x='sepal length',y='sepal width',hue='class',style='class', size='petal width')
+
+sns.relplot(hab_dat,x='Age',y='Nodes detected',hue='Survival status',style='Survival status',size='Nodes detected')
+plt.show()

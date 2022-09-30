3bb411101e0afdb2703d5f70b14dd4bb4fce7bae
Duarte <duarte3391@gmail.com>
2022-09-30 15:16:26 +0100

    'duarte'

--- Lab4/Lab4_duarte.py
+++ Lab4/Lab4_duarte.py
@@ -5,6 +5,7 @@ from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 import sklearn.metrics as met
+from sklearn.model_selection import cross_val_score
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -23,18 +24,25 @@ iris_dat.columns = ['sepal length','sepal width','petal length','petal width','c
 
 
 #Plots
-# sns.set_theme(style="darkgrid")
-# sns.relplot(iris_dat,x='sepal length',y='sepal width',hue='class',style='class', size='petal width')
+sns.set_theme(style="darkgrid")
+sns.relplot(iris_dat,x='sepal length',y='sepal width',hue='class',style='class', size='petal width')
 
-# sns.relplot(hab_dat,x='Age',y='Nodes detected',hue='Survival status',style='Survival status',size='Nodes detected')
+sns.relplot(hab_dat,x='Age',y='Nodes detected',hue='Survival status',style='Survival status',size='Nodes detected')
 # plt.show()
 
-accuracy = {'bayes':[],'SVC':[],'svm':[],'k_neighbors':[]}
-precision = {'bayes':[],'SVC':[],'svm':[],'k_neighbors':[]}
-recall = {'bayes':[],'SVC':[],'svm':[],'k_neighbors':[]}
+
+
+
+accuracy = {'bayes':[],'LinearSVC':[],'svc':[],'k_neighbors':[]}
+precision = {'bayes':[],'LinearSVC':[],'svc':[],'k_neighbors':[]}
+recall = {'bayes':[],'LinearSVC':[],'svc':[],'k_neighbors':[]}
 for i in range(1):
-    X_train, X_test, y_train, y_test = train_test_split(hab_dat.iloc[:,:3].values,hab_dat.iloc[:,3].values, test_size=0.20,shuffle=True )
+    X_train, X_test, y_train, y_test = train_test_split(hab_dat.iloc[:,:3].values,hab_dat.iloc[:,3].values, test_size=0.1,random_state=42 )
+    # train_hab = pd.DataFrame({'Age':[X_train[:,0],y_train[:,0]], 'Year of operation', 'Nodes detected', 'Survival status'})
+
+    # X_train, X_validation, y_train, y_validation = train_test_split(hab_dat.iloc[:,:3].values,hab_dat.iloc[:,3].values, test_size=.2,random_state=30)
     
+
     # Naive Bayes
     bayes_clf = GaussianNB()
     bayes_clf.fit(X_train, y_train)
@@ -45,22 +53,22 @@ for i in range(1):
     # conf_mat['bayes'] = met.confusion_matrix(y_test,y_bayes)
 
     # LinearSVC
-    SVC_clf = LinearSVC()
-    SVC_clf.fit(X_train, y_train)
-    y_SVC = SVC_clf.predict(X_test)
-    accuracy['SVC'].append(met.accuracy_score(y_test,y_SVC))
-    precision['SVC'].append(met.precision_score(y_test,y_SVC))
-    recall['SVC'].append(met.recall_score(y_test,y_SVC))
-    # conf_mat = met.confusion_matrix(y_test,y_SVC)
+    LinearSVC_clf = LinearSVC()
+    LinearSVC_clf.fit(X_train, y_train)
+    y_LinearSVC = LinearSVC_clf.predict(X_test)
+    accuracy['LinearSVC'].append(met.accuracy_score(y_test,y_LinearSVC))
+    precision['LinearSVC'].append(met.precision_score(y_test,y_LinearSVC))
+    recall['LinearSVC'].append(met.recall_score(y_test,y_LinearSVC))
+    # conf_mat = met.confusion_matrix(y_test,y_LinearSVC)
 
-    # SVM
-    # svm_clf = svm()
-    # svm_clf.fit(X_train, y_train)
-    # y_svm = svm_clf.predict(X_test)
-    # accuracy['svm'].append(met.accuracy_score(y_test,y_svm))
-    # precision['svm'].append(met.precision_score(y_test,y_svm))
-    # recall['svm'].append(met.recall_score(y_test,y_svm))
-    # conf_mat = met.confusion_matrix(y_test,y_svm)
+    # SVC
+    svc_clf = svm.SVC()
+    svc_clf.fit(X_train, y_train)
+    y_svc = svc_clf.predict(X_test)
+    accuracy['svc'].append(met.accuracy_score(y_test,y_svc))
+    precision['svc'].append(met.precision_score(y_test,y_svc))
+    recall['svc'].append(met.recall_score(y_test,y_svc))
+    # conf_mat = met.confusion_matrix(y_test,y_svc)
 
     # K-Neighbors
     k_neighbors_clf = KNeighborsClassifier()
@@ -73,6 +81,7 @@ for i in range(1):
     
 
 
+# def cross
 
 
 # print(conf_mat)

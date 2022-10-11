3e7f24efc947b58967b02105942a73bb65bb391b
GoncaloMesquita <goncalos.mesq543@gmail.com>
2022-10-11 15:19:23 +0100

    Project1.1

--- Project/Project1.py
+++ Project/Project1.py
@@ -22,9 +22,14 @@ def data_visualization(data, data_clean, coll):
     """ data[coll].plot()
     plt.title(coll)
     plt.show() """
-    fig, axs = plt.subplots(2)
+    fig, axs = plt.subplots(1,2)
+    fig.suptitle(coll)
     axs[0].plot(data[coll])
-    axs[1].plot(data_clean[coll])
+    axs[0].set_title('histogram without outliers')
+    axs[1].hist(data[coll])
+    axs[1].set_title( 'histogram with outliers')
+    #axs[2].plot(data[coll])
+    #axs[2].set_title('with outliers')
     #fig.title(coll)
     plt.show()
 
@@ -48,7 +53,7 @@ def replacing_outliers_missvalues(data, outlier,miss_value, coll):
 
 def split_data(x, y):
 
-    Xtrain, Xtest, ytrain, ytest = train_test_split(x,y, test_size=0.10,shuffle=True )
+    Xtrain, Xtest, ytrain, ytest = train_test_split(x,y, test_size=0.15,shuffle=True, random_state=5 )
     return Xtrain, Xtest, ytrain, ytest
 
 def normalize_data(xtrain, xtest):
@@ -66,31 +71,111 @@ def balance_data(x, y):
     X, Y = oversample.fit_resample(x, y)
     return X, Y
 
-def cross_validation(xtraining, ytraining):
-    start_time1 = time.process_time()
-    clf = MLPClassifier( hidden_layer_sizes=(6, ),max_iter = 300, activation='relu', random_state=1, early_stopping=True)
-    results = cross_validate(clf,X=xtraining,y=ytraining,cv=5, return_train_score=True,scoring='recall_macro'  ,return_estimator=True)
-    print ("Time of crossvalidation ", time.process_time() - start_time1, "seconds")
-    print('Cross Validation results: ', np.mean(results['test_score']),'\nCross Validation train results: ', np.mean(results['train_score']))
-    return results
-
 def hyperparameters_tunning(x_training, y_training):
-    parameters={ 'hidden_layer_sizes':[(3,),(6,),(6,2),(10,), (13,) ], 'activation':[ 'logistic',  'relu'], 'alpha':[  0.001, 0.0005], 'learning_rate_init':[0.004, 0.003,0.002, 0.001]}
+    parameters={ 'hidden_layer_sizes':[(6,),(6,2), (10,), (14,) ], 'activation':[  'relu'], 'alpha':[  0.001, 0.0005, 0.0001], 'learning_rate_init':[ 0.003,0.002, 0.001]}
     scoring = {'accuracy':met.make_scorer(met.accuracy_score)}
     model = MLPClassifier(early_stopping=True)
     gs_cv = GridSearchCV(model,parameters,scoring=scoring, cv= 5,refit="accuracy")
     gs_cv.fit(x_training,y_training)
     best_params = gs_cv.best_params_
     print(best_params)
+    
+def prediction_multiclass(xtrain, ytrain, xtest, ytest):
 
-def prediction(modelos, X_test, Y_test):
+    accuracy = {'model 1':[],'model 2':[],'model 3':[],'model 4':[]}
+    precision = {'model 1':[],'model 2':[],'model 3':[],'model 4':[]}
+    recall = {'model 1':[],'model 2':[],'model 3':[],'model 4':[]}
+    Time = {'model 1':[],'model 2':[],'model 3':[],'model 4':[]}
+    
+    start_time1 = time.process_time()
+    model1 = MLPClassifier(alpha=0.001, hidden_layer_sizes=(6,), learning_rate_init=0.02)
+    model1.fit(xtrain, ytrain)
+    y_pred1 = model1.predict(xtest)
+    Time['model 1'].append(time.process_time() - start_time1)
+    accuracy['model 1'].append(met.accuracy_score(ytest,y_pred1))
+    precision['model 1'].append(met.precision_score(ytest, y_pred1,  average=None))
+    recall['model 1'].append(met.recall_score(ytest,y_pred1,  average='macro'))
 
-    y_pred = modelos['estimator'][4].predict(X_test)
-    print('Test Accuracy: ' ,met.accuracy_score(Y_test,y_pred))
-    print('Test Precision of each class: ',met.precision_score(Y_test, y_pred,  average=None))
-    print('Test Recall: ', met.recall_score(y_test,y_pred,  average='macro'))
-    print('Confusion Matrix' ,met.confusion_matrix(Y_test,y_pred))
+    start_time2 = time.process_time()
+    model2 = MLPClassifier(alpha=0.0005, hidden_layer_sizes=(10,), learning_rate_init=0.004)
+    model2.fit(xtrain, ytrain)
+    y_pred2 = model2.predict(xtest)
+    Time['model 2'].append(time.process_time() - start_time2)
+    accuracy['model 2'].append(met.accuracy_score(ytest,y_pred2))
+    precision['model 2'].append(met.precision_score(ytest, y_pred2,  average='macro'))
+    recall['model 2'].append(met.recall_score(ytest,y_pred2,  average='macro'))
 
+    start_time3 = time.process_time()
+    model3 = MLPClassifier(alpha=0.0005, hidden_layer_sizes=(14,), learning_rate_init=0.01)
+    model3.fit(xtrain, ytrain)
+    y_pred3 = model3.predict(xtest)
+    Time['model 3'].append(time.process_time() - start_time3)
+    accuracy['model 3'].append(met.accuracy_score(ytest,y_pred3))
+    precision['model 3'].append(met.precision_score(ytest, y_pred3,  average='macro'))
+    recall['model 3'].append(met.recall_score(ytest,y_pred3,  average='macro'))
+
+    start_time4 = time.process_time()
+    model4 = MLPClassifier(alpha=0.0005, hidden_layer_sizes=(6,2), learning_rate_init=0.02)
+    model4.fit(xtrain, ytrain)
+    y_pred4 = model4.predict(xtest)
+    Time['model 4'].append(time.process_time() - start_time4)
+    accuracy['model 4'].append(met.accuracy_score(ytest,y_pred4))
+    precision['model 4'].append(met.precision_score(ytest, y_pred4,  average='macro'))
+    recall['model 4'].append(met.recall_score(ytest,y_pred4,  average='macro'))
+    print('Multiclass Classification')
+    print('accuracy: ', accuracy)
+    print('precision: ', precision)
+    print('recall: ', recall)
+    print('Time: ', Time)
+    return
+
+def prediction_binary(xtrain, ytrain, xtest, ytest):
+    accuracy = {'model 1':[],'model 2':[],'model 3':[],'model 4':[]}
+    precision = {'model 1':[],'model 2':[],'model 3':[],'model 4':[]}
+    recall = {'model 1':[],'model 2':[],'model 3':[],'model 4':[]}
+    Time = {'model 1':[],'model 2':[],'model 3':[],'model 4':[]}
+    
+    start_time1 = time.process_time()
+    model1 = MLPClassifier(alpha=0.0005, hidden_layer_sizes=(6,2), learning_rate_init=0.003)
+    model1.fit(xtrain, ytrain)
+    y_pred1 = model1.predict(xtest)
+    Time['model 1'].append(time.process_time() - start_time1)
+    accuracy['model 1'].append(met.accuracy_score(ytest,y_pred1))
+    precision['model 1'].append(met.precision_score(ytest, y_pred1,  average='macro'))
+    recall['model 1'].append(met.recall_score(ytest,y_pred1,  average='macro'))
+
+    start_time2 = time.process_time()
+    model2 = MLPClassifier(alpha=0.0005, hidden_layer_sizes=(10,), learning_rate_init=0.003)
+    model2.fit(xtrain, ytrain)
+    y_pred2 = model2.predict(xtest)
+    Time['model 2'].append(time.process_time() - start_time2)
+    accuracy['model 2'].append(met.accuracy_score(ytest,y_pred2))
+    precision['model 2'].append(met.precision_score(ytest, y_pred2,  average='macro'))
+    recall['model 2'].append(met.recall_score(ytest,y_pred2,  average='macro'))
+
+    start_time3 = time.process_time()
+    model3 = MLPClassifier(alpha=0.001, hidden_layer_sizes=(6,2), learning_rate_init=0.002)
+    model3.fit(xtrain, ytrain)
+    y_pred3 = model3.predict(xtest)
+    Time['model 3'].append(time.process_time() - start_time3)
+    accuracy['model 3'].append(met.accuracy_score(ytest,y_pred3))
+    precision['model 3'].append(met.precision_score(ytest, y_pred3,  average='macro'))
+    recall['model 3'].append(met.recall_score(ytest,y_pred3,  average='macro'))
+
+    start_time4 = time.process_time()
+    model4 = MLPClassifier(alpha=0.0005, hidden_layer_sizes=(14,), learning_rate_init=0.003)
+    model4.fit(xtrain, ytrain)
+    y_pred4 = model4.predict(xtest)
+    Time['model 4'].append(time.process_time() - start_time4)
+    accuracy['model 4'].append(met.accuracy_score(ytest,y_pred4))
+    precision['model 4'].append(met.precision_score(ytest, y_pred4,  average='macro'))
+    recall['model 4'].append(met.recall_score(ytest,y_pred4,  average='macro'))
+    print('\n Binary Classification')
+    print('accuracy: ', accuracy)
+    print('precision: ', precision)
+    print('recall: ', recall)
+    print('Time: ', Time)
+    return
 #---------------------MAIN-----------------------------------
 
 df = pd.read_csv('Proj1_Dataset.csv')
@@ -103,7 +188,7 @@ df = pd.read_csv('Proj1_Dataset.csv')
 #print(df.describe())
 
 df_clean = df.copy()
-df_clean.drop(['Date', 'Time'], axis=1, inplace=True)
+df_clean.drop(['Date', 'Time'] , axis=1, inplace=True)
 for i in df_clean.columns:
     
     #if i != "Time" and i !="Date":  
@@ -145,17 +230,26 @@ x_norm_test, x_norm_train = normalize_data(x_train, x_test)
 
 #counter = Counter(y_train)
 #print(counter)
+
 X_train, Y_train = balance_data(x_norm_train, y_train)
+y_train_binary = np.where(Y_train>2, 1, 0 )
+y_test_binary = np.where(y_test>2, 1, 0 )
 
-#CROSS VALIDATION 
 
-#models = cross_validation(X_train, Y_train)
+""" plt.figure(figsize=(12,10), dpi= 80)
+sns.heatmap(X_train.corr(), xticklabels=X_train.corr().columns, yticklabels=X_train.corr().columns, cmap='RdYlGn', center=0, annot=True)
+plt.title('Correlogram heatmap', fontsize=22)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+plt.show() """
+
 
 #Hyperparameters tunning
 
-hyperparameters_tunning(X_train,Y_train )
+#hyperparameters_tunning(X_train,Y_train )
+#hyperparameters_tunning(X_train, y_train_binary )
 
 #PREDICTION RESULTS
 
-#prediction(models,x_norm_test, y_test)
-
+prediction_multiclass(X_train, Y_train, x_norm_test,y_test )
+prediction_binary(X_train, y_train_binary,x_norm_test,y_test_binary  )

082aa628ede627472e2b1b32648a59f05d5253be
GoncaloMesquita <goncalos.mesq543@gmail.com>
2022-10-27 18:31:33 +0100

    abc

--- Project_2/Project2.py
+++ Project_2/Project2.py
@@ -7,56 +7,34 @@ from deap import tools
 import random
 from more_itertools import one
 
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMin)
-n_ind = 51
-n = np.arange(n_ind)
-toolbox = base.Toolbox()
-toolbox.register('Genes', random.sample, range(n_ind),n_ind )
-toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.Genes)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-def evaluate(individuals):
-    value=[]
-    value.append(cost_distance.values[0,individuals[0]])
-    for i in range(n_ind-2):
-        value.append(cost_distance.values[individuals[i],individuals[i+1]])
-    value.append(cost_distance.values[individuals[n_ind-1],0])
-    fit_ind = (sum(value))
-    return fit_ind,
+def minimization():
 
-toolbox.register("evaluate", evaluate)
-toolbox.register("mate", tools.cxTwoPoint )
-toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.7)  
-toolbox.register("select", tools.selTournament, tournsize=4)
-
-def minimization(distance):
     random.seed(64)
-    n=100
-    CXPB , MUTPB = 0.6, 0.4
-    pop = toolbox.population(100)
-    print(pop)
+    n_pop=100
+    CXPB , MUTPB = 0.6, 0.6
+    pop = toolbox.population(n_pop)
     fitnesses = list(map(toolbox.evaluate, pop))
     for ind, fit in zip(pop, fitnesses):
         ind.fitness.values = fit
     
     g=0
-    while g < 50:
-    
+    while g < 100:
+        
         g = g + 1
-
+        print('Genration ', g)
         offspring = toolbox.select(pop, len(pop))  
         offspring = list(map(toolbox.clone, offspring))
 
         # CROSSOVER
-
+        """ 
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
 
             if random.random() < CXPB:
                 toolbox.mate(child1, child2)
                 del child1.fitness.values
                 del child2.fitness.values 
-        
+         """
         #Mutation
 
         for mutant in offspring:
@@ -74,30 +52,80 @@ def minimization(distance):
 
         for ind, fit in zip(invalid_ind, fitnesses):
             ind.fitness.values = fit
-            print(ind.fitness.values[0])
-
-
+            
         pop[:] = offspring
 
+        fits = [ind.fitness.values[0] for ind in pop]
+        
+        length = len(pop)
+        mean = sum(fits) / length
+        sum2 = sum(x*x for x in fits)
+        std = abs(sum2 / length - mean**2)**0.5
+    
+        print("  Min %s" % min(fits))
+        print("  Max %s" % max(fits))
+        print("  Avg %s" % mean)
+        print("  Std %s" % std) 
+        #fit = np.array(fits)
+        #best_10 = fit.argsort()
+        #print("-- End of (successful) evolution --")
+        best_ind = tools.selBest(pop, 1)[0]
+        print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values)) 
 
+    count = 0
+    cost=[]
+    for i in (best_ind):
+        if i == 0 :
+            cost.append(count)
+            count = 0
+        count = orders['Orders'][i]+count
+    cost.append(count)
+    print("Contagem  ida", cost )
+        
 
     
 
     return
 
+def evaluate(individuals):
+    value=[]
+    value.append(cost_distance.values[0,individuals[0]])
+    for i in range(n_ind):
+        value.append(cost_distance.values[individuals[i],individuals[i+1]])
+    value.append(cost_distance.values[individuals[n_ind],0])
+    fit_ind = (sum(value))
+    return fit_ind,
 
 ##################################  MAIN  ############################################
+n_ind = 51
+indiv = np.arange(n_ind)
 
-n = np.arange(n_ind)
-
-cost_distance = pd.read_csv('CustDist_WHCentral.csv', header= 0, names= n)
+cost_distance = pd.read_csv('CustDist_WHCentral.csv', header= 0, names = indiv)
 location = pd.read_csv('CustXY_WHCentral.csv')
 orders = pd.read_csv('CustOrd.csv')
-#print(cost_distance)
-""" print(location)
-print(orderes)  """
+zeros = np.zeros(1, dtype=int)
+costumers = np.append(indiv,zeros)
 
 plt.scatter(x=location['X'][1:], y=location['Y'][1:], marker='x')
 plt.scatter(x=location['X'][0], y=location['Y'][0])
 plt.show()
-minimization(cost_distance)
+
+################################  MINIMIZATION CLASS  ###################################
+
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMin)
+
+###############################  CREATING POPULATION  ######################################
+
+toolbox = base.Toolbox()
+toolbox.register('Genes', np.random.permutation, costumers )
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.Genes)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+toolbox.register("evaluate", evaluate)
+toolbox.register("mate", tools.cxPartialyMatched)
+toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.7)  
+toolbox.register("select", tools.selTournament, tournsize=4)
+
+
+minimization()
